4fe5183195168145535203dd4fac4f60b7073e61
phattran <phattran052004@gmail.com>
2025-09-08 20:59:35 +0700

    implement login flow with token storage and refresh token interceptor

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,16 @@ import 'pages/home_page.dart';
 import 'pages/signin_page.dart';
 import 'pages/signup_page.dart';
 import 'pages/main_layout.dart';
+import 'apis/api_client.dart';
 
 void main() {
-  runApp(
-    DevicePreview(
-      builder: (context) => const MyApp(),
-    ),
-  );
+  ApiClient.init();
+  runApp(DevicePreview(builder: (context) => const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,12 +25,11 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const WelcomePage(),
-      routes: 
-      {
+      routes: {
         '/home': (context) => const HomePage(),
         '/main': (context) => const MainLayout(),
         '/signin': (context) => const SignInPage(),
-        '/signup': (context) => const SignUpPage(), 
+        '/signup': (context) => const SignUpPage(),
       },
     );
   }
