fb4533dc953d6b34fe571699907034310160a744
Tran Vi <vitren63666@gmail.com>
2025-09-14 09:58:53 +0700

    api(product): get product & create product

--- lib/config/api_constants.dart
+++ lib/config/api_constants.dart
@@ -4,8 +4,6 @@ import 'package:flutter/foundation.dart' show kIsWeb, defaultTargetPlatform, Tar
 class ApiConstants {
   static String get baseUrl {
     final isAndroid = !kIsWeb && defaultTargetPlatform == TargetPlatform.android;
-    // Nếu chạy Android Emulator → dùng 10.0.2.2
-    // Nếu chạy iOS Simulator, Web, Desktop → dùng localhost
     return isAndroid ? 'http://10.0.2.2:4000/api/v1' : 'http://localhost:4000/api/v1';
   }
 }
